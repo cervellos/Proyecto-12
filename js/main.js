f8f40a15c153a58c48d8adbd82e32c1402f8469a
Freddy Guerra <arcobaleno262@gmail.com>
2022-07-26 22:23:23 -0300

    punto 3 del ejercicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 console.log(document.querySelector("title").innerText);
+
+const listaAlum = document.getElementById("lista").textContent;
+console.log(listaAlum);
+
+const [
+  titulo1,
+  campoPrimN1,
+  primNombre1,
+  campoSegN1,
+  segNombre1,
+  campoPrimA1,
+  primApellido1,
+  campoSegA1,
+  segApellido1,
+  titulo2,
+  campoprimN2,
+  primNombre2,
+  campoSegN2,
+  campoPrimA2,
+  primApellido2,
+  campoSegA2,
+] = listaAlum;
+
+console.log(
+  listaAlum[0] +
+    '1: "' +
+    primNombre1 +
+    " " +
+    segNombre1 +
+    " " +
+    primApellido1 +
+    " " +
+    segApellido1 +
+    '"'
+);
